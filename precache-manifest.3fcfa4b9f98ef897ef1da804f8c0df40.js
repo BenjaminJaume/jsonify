58579af01b5742d89269d19283a6e68a3a05f335
BenjaminJaume <47286801+BenjaminJaume@users.noreply.github.com>
2020-02-12 10:17:36 -0600

    Updates

---
+++ precache-manifest.3fcfa4b9f98ef897ef1da804f8c0df40.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0f7dd61092684c644a8ec4265424f29",
+    "url": "/jsonify/index.html"
+  },
+  {
+    "revision": "06e69d42ba54e3a3d922",
+    "url": "/jsonify/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b33dff2f7522490c0a20",
+    "url": "/jsonify/static/js/2.a7de84f8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/jsonify/static/js/2.a7de84f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06e69d42ba54e3a3d922",
+    "url": "/jsonify/static/js/main.326ef82e.chunk.js"
+  },
+  {
+    "revision": "c62f4d1395347882c2f4",
+    "url": "/jsonify/static/js/runtime-main.cc9e9501.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jsonify/static/media/logo.5d5d9eef.svg"
+  }
+]);
